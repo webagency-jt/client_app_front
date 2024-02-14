10753719fd6d06246c8fd2545525e008676f8408
LucasReckall <lucas@reckall.com>
2024-02-14 12:39:54 +0100

    add connection login with api

--- src/app/lib/actions.tsx
+++ src/app/lib/actions.tsx
@@ -4,18 +4,23 @@
 
 import {StatusCodes} from "http-status-codes";
 
-export async function authenticate(user:IUser) {
-    const statusCode = await signIn(user);
-        if (statusCode) {
-            switch (statusCode) {
-                case StatusCodes.BAD_REQUEST:
-                    return 'Invalid credentials.'
+export async function authenticate(user: IUser) {
+    try {
+        // Appel à la fonction signIn pour obtenir les données de la réponse
+        const response = await signIn(user);
+        // Vérification du contenu de la réponse
+        if (response.statusCode === StatusCodes.OK ) {
+            return response;
+        } else {
+            switch (response.statusCode) {
                 case StatusCodes.NOT_FOUND:
-                    return 'Invalid Credentials'
-                case StatusCodes.OK:
-                    return 'Success'
+                    return {statusCode : StatusCodes.NOT_FOUND , data:'Invalid credentials'};
             }
         }
+    } catch (error) {
+        // Si une erreur est survenue lors de l'appel à signIn, renvoyer un message d'erreur générique
+        return {statusCode : StatusCodes.NOT_FOUND , data: 'An error occurred.'};
+    }
 }
 
 export async function signIn(user: IUser) {
@@ -26,7 +31,7 @@ export async function signIn(user: IUser) {
         },
         body: JSON.stringify(user),
     });
-    return res.status
+    return { statusCode: res.status, data: await res.json() };
 }
 
 export interface IUser {
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
 'use client'
-
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {authenticate, IUser} from '@/app/lib/actions';
 import {useFormStatus} from 'react-dom';
 import {useRouter} from "next/navigation";
 import Image from "next/image"
 import {useState} from "react";
+import {StatusCodes} from "http-status-codes";
 
 export default function Page() {
     return (
@@ -14,7 +16,7 @@ export default function Page() {
                     <h2 className="mt-6 text-center text-4xl font-extrabold text-gray-900">
                         Connexion
                     </h2>
-                    <LoginForm />
+                    <LoginForm/>
                 </div>
                 <div>
                     <Image src='/auth.svg' priority={true} alt='auth' width='564' height='496' className="w-94 h-auto"/>
@@ -28,13 +30,14 @@ function LoginForm() {
     const {pending} = useFormStatus();
     const router = useRouter();
     const [passwordVisible, setPasswordVisible] = useState(false);
+    const [error, setError] = useState('');
 
     const togglePasswordVisibility = () => {
         setPasswordVisible(!passwordVisible);
     };
 
     async function handleSubmit(formData: FormData) {
-
+        setError('');
         const email = formData.get('email') ?? ''
         const password = formData.get('password') ?? ''
         const user: IUser = {
@@ -42,50 +45,71 @@ function LoginForm() {
             password: password.toString()
         }
         const response = await authenticate(user);
-        console.log(response)
+        if (response?.statusCode === StatusCodes.NOT_FOUND) {
+            showErrorToast(response.data);
+        } else{
+            router.push('/dashboard');
+        }
 
+    };
 
-        //router.push('/dashboard')
+    const showErrorToast = (error: string) => {
+        toast.error(error, {
+            position: 'bottom-left', // Position du toast en bas à gauche
+            autoClose: 5000, // Durée d'affichage du toast en millisecondes
+            hideProgressBar: true, // Masquer la barre de progression
+            closeOnClick: true, // Fermer le toast lorsqu'il est cliqué
+            pauseOnHover: true, // Mettre en pause le timer lorsqu'on survole le toast
+            draggable: true, // Permettre de faire glisser le toast
+            progress: undefined, // Paramètre de progression (peut être personnalisé si nécessaire)
+            theme: "colored",
+            style: {
+                backgroundColor: 'red', // Couleur de fond rouge
+            },
+        });
     };
 
     return (
-        <form className="mt-8 space-y-6" action={handleSubmit}>
-            <input
-                type="email"
-                name="email"
-                placeholder="Enter Email"
-                required
-                className="appearance-none rounded-none relative block w-full px-4 py-2 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm bg-blue-100 bg-opacity-50"
-            />
-            <div className="relative">
+        <>
+            <form className="mt-8 space-y-6" action={handleSubmit}>
                 <input
-                    type={passwordVisible ? 'text' : 'password'}
-                    name="password"
-                    placeholder="Password"
+                    type="email"
+                    name="email"
+                    placeholder="Enter Email"
                     required
                     className="appearance-none rounded-none relative block w-full px-4 py-2 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm bg-blue-100 bg-opacity-50"
                 />
+                <div className="relative">
+                    <input
+                        type={passwordVisible ? 'text' : 'password'}
+                        name="password"
+                        placeholder="Password"
+                        required
+                        className="appearance-none rounded-none relative block w-full px-4 py-2 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm bg-blue-100 bg-opacity-50"
+                    />
+                    <button
+                        type="button"
+                        onClick={togglePasswordVisibility}
+                        className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                    >
+                        {passwordVisible ? (
+                            <Image src='/eye.svg' alt='passwordNoVisible' width='19' height='16'></Image>
+                        ) : (
+                            <Image src='/eyecrossed.svg' alt='passwordVisible' width='19' height='16'></Image>
+                        )}
+                    </button>
+                </div>
                 <button
-                    type="button"
-                    onClick={togglePasswordVisibility}
-                    className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                    type="submit"
+                    className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+                        pending && 'opacity-50 cursor-not-allowed'
+                    }`}
+                    aria-disabled={pending}
                 >
-                    {passwordVisible ? (
-                        <Image src='/eye.svg' alt='passwordNoVisible' width='19' height='16'></Image>
-                    ) : (
-                        <Image src='/eyecrossed.svg' alt='passwordVisible' width='19' height='16'></Image>
-                    )}
+                    {pending ? 'Logging in...' : 'Sign in'}
                 </button>
-            </div>
-            <button
-                type="submit"
-                className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
-                    pending && 'opacity-50 cursor-not-allowed'
-                }`}
-                aria-disabled={pending}
-            >
-                {pending ? 'Logging in...' : 'Sign in'}
-            </button>
-        </form>
+            </form>
+            <ToastContainer/>
+        </>
     );
 }
