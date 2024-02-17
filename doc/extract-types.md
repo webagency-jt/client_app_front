d42feb3afe158892466e6f16ef43aa4c0f4c5807
Mathys <mathystheolade973@gmail.com>
2024-02-17 20:45:55 +0100

    add interface generator

---
+++ doc/extract-types.md
@@ -0,0 +1,7 @@
+### Extration
+Pour générer l'interface proposé par le back il faut lancer la commande suivant
+```powershell
+npx openapi -i http://localhost:3000/doc -o generated/
+```
+
+Pour plus d'informations voici la [documentation](https://www.npmjs.com/package/openapi-typescript-codegen)
---
+++ generated/models/UserInformations.ts
@@ -0,0 +1,18 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type UserInformations = {
+    id: string;
+    userId: string;
+    address: string;
+    city: string;
+    firstname: string;
+    lastname: string;
+    phoneNumber: string;
+    siret: string;
+    state: string;
+    tva: string;
+    zip: string;
+};
+
